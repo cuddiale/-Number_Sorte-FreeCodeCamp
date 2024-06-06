a5c441091ef4f0153f58af1376731de18f78e8a1
cuddiale <alessiocuddretto@hotmail.com>
2024-06-06 09:31:02 +0200

    Update

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = selectionSort(inputValues);
+
+  updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
@@ -13,6 +17,33 @@ const updateUI = (array = []) => {
     const outputValueNode = document.getElementById(`output-value-${i}`);
     outputValueNode.innerText = num;
   });
+};
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+};
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array, array[j], array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+  }
 };
 
 sortButton.addEventListener("click", sortInputArray);
