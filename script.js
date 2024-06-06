29542bead480e2687dba57b76dcff9fcc7e06203
cuddiale <alessiocuddretto@hotmail.com>
2024-06-06 09:26:37 +0200

    Update

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
+
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown"),
   ].map((dropdown) => Number(dropdown.value));
-  console.log(inputValues);
+};
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
 };
 
 sortButton.addEventListener("click", sortInputArray);
